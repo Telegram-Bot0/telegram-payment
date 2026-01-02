7b4889c3b5e5b25a241d53f6dc36ab3c40a326ba
Telegram-Bot0 <donr5432112345@gmail.com>
2026-01-03 00:11:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7998689373:AAGUaTxrHzWabJvqZAUM2NjRQb1PHXpmGqA"
+MONGO_URI = "mongodb+srv://telegram-payment-bot:Jatin161@telegram-payment-bot.cabji2f.mongodb.net/?appName=telegram-payment-bot"
+ADMIN_ID = 7495749312
