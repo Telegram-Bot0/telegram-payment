d4c1c80b37125f2b448d6e57702e65e83137c3cb
Telegram-Bot0 <donr5432112345@gmail.com>
2026-01-03 23:09:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ DEPOSIT_PENDING_GROUP_ID = -1003415130807
 DEPOSIT_COMPLETED_GROUP_ID = -1003513719508
 WITHDRAW_REQUESTS_GROUP_ID = -1003522192758  # Use deposit group for now
 WITHDRAW_COMPLETED_GROUP_ID = -1003637541728  # Use deposit group for now
-MONGO_URI = "mongodb+srv://telegram-payment-bot:Jatin161@telegram-payment-bot.cabji2f.mongodb.net/telegram_payment_bot?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://telegram-payment-bot:Jatin161@telegram-payment-bot.cabji2f.mongodb.net/?retryWrites=true&w=majority&serverSelectionTimeoutMS=10000&connectTimeoutMS=10000&socketTimeoutMS=10000"
